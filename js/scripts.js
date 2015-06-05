dcdff13b8e0c70406d06c09d6b4a9c471123500f
Chris Le Compte <christgopher@gmail.com>
2015-06-05 12:58:59 -0700

    add web functionality

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,59 @@ function totalCost(pizzas) {
   })
   return cost;
 }
+
+$(document).ready(function() {
+  $("#add-pizza").click(function() {
+    $("#new-pizzas").append('<div class="new-pizza">' +
+                              '<div class="form-group">' +
+                              '<label for="new-size">Size:</label>' +
+                              '<select class="form-control new-size" name="size" required>' +
+                                '<option selected disabled hidden value="">Please choose</option>' +
+                                '<option value="small">Small</option>' +
+                                '<option value="medium">Medium</option>' +
+                                '<option value="large">Large</option>' +
+
+                              '</select>' +
+                            '</div>' +
+
+                            '<div class="form-group">' +
+                              '<label for="new-toppings">Toppings:</label>' +
+                              '<select multiple class="form-control new-toppings" name="toppings[]" size=5 required>' +
+                                '<option value="pepperoni">Pepperoni</option>' +
+                                '<option value="sausauge">Sausauge</option>' +
+                                '<option value="olives">Olives</option>' +
+                                '<option value="onions">Onions</option>' +
+                                '<option value="mushrooms">Mushrooms</option>' +
+                                '</select>' +
+                              '</div>' +
+                            '</div>');
+  });
+
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+    pizzas = []
+
+    $(".new-pizza").each(function() {
+      var inputtedToppings = $(this).find(".new-toppings").val();
+      var inputtedSize = $(this).find(".new-size").val();
+      var newPizza = new Pizza(inputtedSize, inputtedToppings);
+      pizzas.push(newPizza)
+    });
+    pizzas.forEach(function(pizza) {
+      $("ul#pizzas").append("<li>" + pizza.size + " pizza with " + pizza.ingredients + "." + "</li>");
+
+    });
+
+  });
+
+  $("button#checkout-button").click(function() {
+    $("#order").fadeOut();
+    $("#total").text(totalCost(pizzas));
+    $("#checkout").delay(750).fadeIn();
+
+  })
+
+
+
+
+});
