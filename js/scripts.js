af2ecb12df0959f686458cb6575c3710ebd36fc1
Chris Le Compte <christgopher@gmail.com>
2015-06-05 10:51:55 -0700

    add cost function for pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function Pizza(size, ingredients) {
   this.size = size;
   this.ingredients = ingredients;
 }
+
+Pizza.prototype.pizzaCost = function() {
+  var cost = 6;
+  if (this.size === "medium") {
+    cost += 3;
+  } else if (this.size === "large") {
+    cost += 6;
+  }
+
+  return (cost + this.ingredients.length);
+}
--- spec/specs.js
+++ spec/specs.js
@@ -2,6 +2,11 @@ describe('Pizza', function() {
   it('has the properties size and ingredients', function() {
     var testPizza = new Pizza("medium", ["sausauge", "mushrooms", "olives"]);
     expect(testPizza.size).to.equal("medium");
-    expect(testPizza.ingredients).to.eql(["sausauge", "mushrooms", "olives"])
+    expect(testPizza.ingredients).to.eql(["sausauge", "mushrooms", "olives"]);
+  })
+
+  it('can determine its own cost and return it', function() {
+    var testPizza = new Pizza("medium", ["sausauge", "mushrooms", "olives"]);
+    expect(testPizza.pizzaCost()).to.equal(12);
   })
 })
